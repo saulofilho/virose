9d601569961c4e2f6c7ca85494e9794381ff11f1
SauloFilho <saulosilvaf@gmail.com>
2019-05-11 13:41:20 -0300

    up
    sat 11 may 11h40

--- sketch.js
+++ sketch.js
@@ -1,3 +1,26 @@
+class Blob {
+
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      let angle = random(0, 2*PI);
+      this.xspeed = random(-30, 15)*Math.cos(angle);
+      this.yspeed = random(-30, 15)*Math.sin(angle);
+      this.r = random(60, 200);
+  }
+
+  update() {
+      this.x += this.xspeed;
+      this.y += this.yspeed;
+      if(this.x > width || this.x < 0) this.xspeed *= -1;
+      if(this.y > height || this.y < 0) this.yspeed *= -1;
+  }
+
+  show() {
+      ellipse(this.x, this.y, this.r*1, this.r*1);
+  }
+}
+
 var blobs = []
 var slider;
 var slider2;
@@ -18,8 +41,8 @@ function setup() {
     slider3 = createSlider(-100, 100, 10, 0.01);
     slider3.parent('myInputs');
     slider3.class('inputs');
-}
-
+  }
+  
 function draw() {
     var val = slider.value();
     var val2 = slider2.value();
